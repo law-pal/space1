7b0aad423cfb4d2e481490e6fbd4e83e1769c500
lawpal86 <67480487+lawpal86@users.noreply.github.com>
2020-07-19 15:32:55 -0500

    'update'

---
+++ script.js
@@ -0,0 +1,20 @@
+let USSship = {
+    Hull: 20,
+    Power: 5,
+    Accuracy: 0.7
+}
+
+
+function AlienShips(Hull, Power, Accuracy){
+    this.Hull = Hull;
+    this.Power = Power;
+    this.Accuracy = Accuracy;
+}
+let AlienShip1 = new AlienShips(5, 3, 0.6);
+let AlienShip2 = new AlienShips(3, 2, 0.8);
+let AlienShip3 = new AlienShips(6, 4, 0.6);
+let AlienShip4 = new AlienShips(4, 4, 0.5);
+let AlienShip5 = new AlienShips(5 ,3, 0.6);
+let AlienShip6 = new AlienShips(3, 2, 0.7);
+
+
